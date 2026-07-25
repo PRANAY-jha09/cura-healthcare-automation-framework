884882c2cff2937dd7b5ce51d49d80c63486715c
PRANAY-jha09 <Pranayjha535@gmail.com>
2026-07-25 16:39:49 +0530

    feat: implement Chrome driver factor

---
+++ core/driver_factor.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from config.config import IMPLICIT_WAIT
+
+class DriverFactory:
+
+    @staticmethod
+    def create_driver():
+
+        driver =webdriver.Chrome(
+            service =Service(ChromeDriverManager().install()
+                             )
+        )
+        driver.maximize_window()
+
+        driver.implicitly_wait(IMPLICIT_WAIT)
+
+        return driver
+
+
+
+
