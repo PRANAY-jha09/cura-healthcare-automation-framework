22213748a7bde18eaaa009ae569bf0f15670286c
PRANAY-jha09 <Pranayjha535@gmail.com>
2026-07-25 16:43:40 +0530

    feat:add base page class

---
+++ core/base_page.py
@@ -0,0 +1,8 @@
+class BasePage:
+
+    def __init__(self,driver):
+        self.driver =driver
+
+
+# later we add click(),type(),wait_for_element().get_text().screenshots,scrolling,javaScript helpers
+
